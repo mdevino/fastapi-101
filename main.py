fdc3643fd40b2ce542e04b6d9e4bb126e712e670
Mateus Devino <mateus@mdevino.com>
2022-03-17 19:44:24 -0300

    Models endpoint

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 from fastapi import FastAPI
 
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 async def root():
   return {'message': 'Hello World'}
 
-@app.get("/items/{item_id}")
+
+@app.get('/items/{item_id}')
 async def read_item(item_id: int):
   return {'item_id': item_id}
+
+
+@app.get('/users/me')
+async def read_current_user():
+  return {'user_id': 'current user'}
+
+@app.get('/users/{user_id}')
+async def read_user(user_id: int):
+  return {'user_id': user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
