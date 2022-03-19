02c8ccf9c1add65f9cde21d27eaa39b3cfc26559
Mateus Devino <mateus@mdevino.com>
2022-03-19 09:22:27 -0300

    All items endpoint

--- src/routers/items.py
+++ src/routers/items.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
 
+fake_items_db = [
+  { 'item_name': 'Foo'},
+  { 'item_name': 'Bar'},
+  { 'item_name': 'Baz'}
+]
 
 router = APIRouter(
   prefix='/items'
 )
+
+@router.get("/")
+async def read_item(skip: int = 0, limit: int = 10):
+  return fake_items_db[skip : skip + limit]
 
 @router.get('/{item_id}')
 async def read_item(item_id: int):
--- src/tests/integration/test_items.py
+++ src/tests/integration/test_items.py
@@ -6,6 +6,22 @@ client = TestClient(app)
 
 class TestItems:
 
+  def test_get_all_items(self):
+    response = client.get('/items/')
+    assert response.status_code == 200
+    assert response.json() == [
+      { 'item_name': 'Foo'},
+      { 'item_name': 'Bar'},
+      { 'item_name': 'Baz'}
+    ]
+
+  def test_get_all_items_given_skip_and_limit(self):
+    response = client.get('/items/?skip=1&limit=1')
+    assert response.status_code == 200
+    assert response.json() == [
+      { 'item_name': 'Bar'}
+    ]
+
   def test_get_item(self):
     response = client.get('/items/2')
     assert response.status_code == 200
